24ec2bcbd9983555f3cfa536ecf18d022b9bd260
Vegascruz <cruzv@students.westerntc.edu>
2023-01-08 15:17:24 -0600

    Added some styling to the home page

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -1,4 +1,12 @@
 import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {
+  AiFillTwitterCircle, 
+  AiFillLinkedin, 
+  AiFillYoutube
+} from "react-icons/ai"
+import Image from "next/image";
+//import deved from '../public/'
 
 export default function Home() {
   return (
@@ -9,9 +17,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main> 
-        <section className="bg-blue-400 min-h-screen">
-          <h1>Hello </h1>
+      <main className='text-black bg-white px-10'> 
+        <section className="min-h-screen">
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons'>Developed by Vegas Cruz</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+              </li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">About</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Contact</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Portfolio</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Vegas Cruz</h2>
+            <h3 className="text-2xl py-2">Full Stack Developer from Michigan State University</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis cumque quaerat rem pariatur! Praesentium fugit odit enim saepe error consectetur tenetur. Consequatur nobis iure maiores sed eveniet ad dignissimos amet?
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle/>
+            <AiFillLinkedin/>
+            <AiFillYoutube/>
+          </div>
+          <div className="">
+            <Image></Image>
+          </div>
         </section>
       </main>
     </>
