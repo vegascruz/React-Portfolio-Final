5fb94698d048b8661895fd561329ad4931630fb5
Vegascruz <cruzv@students.westerntc.edu>
2023-01-14 14:06:07 -0600

    Updated my README, gonna add paragraph titles and seperate it a bit

--- react-portfolio/pages/About.tsx
+++ react-portfolio/pages/About.tsx
@@ -29,6 +29,18 @@ export default function About() {
                 <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="/Resume">Resume</a></li>
               </ul>
             </nav>
+            <div className='text-center p-10'>
+              <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400">About Me!</h2>
+              <h3 className="text-xl py-2">I am from Sparta, WI! If you don't know where that is, it's near La Crosse. Currently, 
+              I live in Milwaukee with my beautiful girlfriend, Amanda Yenter. I am 24 years young. My favorite hobby/passion is Volleyball. I love sand doubles, and grass triples... someday, 
+              I'd like to live in California and play professionally. I also love to workout, my goal someday is a 40" vertical. Lately, I've rekindled my love for gaming. Love Call of Duty, 
+              fighting games Mortal Kombat, and I also love RPG's like God of War and Dark Souls. I also love to watch TV, especially anime. Animes like Naruto, Dragonball, 
+              Haikyuu, Bleach, and One Piece. My second goal someday is to be able to travel more... being a part time worker/student, I don't have the ability but that's a goal.
+              I am currently in search for a Full-Stack or Web Developer job. I love front end, but am growing to understand/like backend more. I fell in love with programming in High School where I took two programming classes.
+              We learned C++ through Visual Studio. We mostly worked on the command line, but I grew to love it the day I programmed a computer to say "Hello World". Let me tell you about my personality a bit. I have the work ethic of a horse, EXCEPT when it comes to doing dishes haha. I'm fun, and have a very bubbly personality. I'm kind and I treat people with respect... my girlfriend says I'm too much of a people pleaser, 
+              but it's how I grew up.  
+               </h3>
+            </div>
             </section>
         </main>
         </div>
